6b2e2e40cf1eeb50db423a3b613d03514fab6040
SunYerim <yaelim6@pukyong.ac.kr>
2025-07-03 15:22:31 +0900

    feat: Initial commit for Spring Boot backend

---
+++ src/main/java/com/connective/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.connective.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
