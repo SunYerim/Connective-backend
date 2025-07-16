78452e7b5bec61b78ec9ad0b5ad7f072e78632e7
SunYerim <yaelim6@pukyong.ac.kr>
2025-07-16 14:28:50 +0900

    feat: #1. CustomUserDetailService구현

--- src/main/java/com/connective/server/user/application/service/CustomUserDetailService.java
+++ src/main/java/com/connective/server/user/application/service/CustomUserDetailService.java
@@ -1,4 +1,36 @@
 package com.connective.server.user.application.service;
 
-public class CustomUserDetailService {
+import com.connective.server.user.domain.entity.User;
+import com.connective.server.user.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userIdString) throws UsernameNotFoundException {
+        Long userId = Long.valueOf(userIdString);
+
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found with id: " + userId));
+
+        // 모든 사용자가 'ROLE_USER' 권한을 가지도록 설정함
+        return org.springframework.security.core.userdetails.User.builder()
+            .username(user.getId().toString())
+            .password("")
+            .authorities("ROLE_USER")
+            .accountExpired(false)
+            .accountLocked(false)
+            .credentialsExpired(false)
+            .disabled(false)
+            .build();
+
+    }
 }
