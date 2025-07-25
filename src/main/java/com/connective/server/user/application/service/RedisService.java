16cc1d6fa99ec352c149f949092daa028c72bfb8
SunYerim <yaelim6@pukyong.ac.kr>
2025-07-25 15:53:12 +0900

    feat: #1. redis서비스 설정

---
+++ src/main/java/com/connective/server/user/application/service/RedisService.java
@@ -0,0 +1,15 @@
+package com.connective.server.user.application.service;
+
+import java.time.Duration;
+
+public interface RedisService {
+
+    void setValues(String key, String value);
+
+    void setValues(String key, String value, Duration duration);
+
+    String getValue(String key);
+
+    void deleteValue(String key);
+
+}
---
+++ src/main/java/com/connective/server/user/domain/dto/auth/LoginResponseDTO.java
@@ -0,0 +1,4 @@
+package com.connective.server.user.domain.dto.auth;
+
+public class LoginResponseDTO {
+}
