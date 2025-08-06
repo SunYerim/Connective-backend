28cde734d9e79312ae1592cf1ad2d9c677a5d584
SunYerim <yaelim6@pukyong.ac.kr>
2025-08-06 17:16:24 +0900

    feat: #1. tokenDTO, Login관련 DTO 추가

--- src/main/java/com/connective/server/user/domain/dto/auth/TokenDTO.java
+++ src/main/java/com/connective/server/user/domain/dto/auth/TokenDTO.java
@@ -1,4 +1,17 @@
 package com.connective.server.user.domain.dto.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class TokenDTO {
+
+    private String accessToken;
+    private String refreshToken;
+
 }
